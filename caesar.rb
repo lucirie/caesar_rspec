49548fa3cd1485a7899b524534508d10f7cafa10
Ziad Alezzi <lucirieko@gmail.com>
2025-11-29 20:08:36 +0200

    caesar cipher function

---
+++ caesar.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+def caesar_cipher(string, key)
+  string_arr = string.chars.map do |char|
+    ascii = char.ord
+    if (ascii >= 65) && (ascii <= 90)
+      char = ((ascii - 65 + key) % 26 + 65).chr
+      char.chr
+    elsif (ascii >= 97) && (ascii <= 122)
+      ((ascii - 97 + key) % 26 + 97).chr
+    else
+      char
+    end
+  end
+  string_arr.join('')
+end
+
+puts caesar_cipher('What a string!', 5) # => "Bmfy f xywnsl!"
